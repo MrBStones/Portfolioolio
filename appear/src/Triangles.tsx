67b9fb422569504d79bdc22d44a5f240e3b36155
MrBStones <70972115+MrBStones@users.noreply.github.com>
2024-09-30 19:21:01 +0200

    Experimenting

---
+++ appear/src/Triangles.tsx
@@ -0,0 +1,16 @@
+import tris from './assets/triangles.svg'
+import './App.css'
+
+function Triangles() {
+
+    return (
+        <>
+            <div className="tris" style={{backgroundImage: `url(${tris})`}}>
+
+            </div>
+            <p>hello world</p>
+        </>
+    )
+}
+
+export default Triangles
