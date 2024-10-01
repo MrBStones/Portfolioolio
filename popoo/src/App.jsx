c066b5b9ab51066a98f6fd27b38a46424fda120b
MrBStones <70972115+MrBStones@users.noreply.github.com>
2024-10-01 20:55:54 +0200

    some jsx

--- popoo/src/App.jsx
+++ popoo/src/App.jsx
@@ -1,37 +1,29 @@
-import { useState } from 'react'
-import reactLogo from '/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import Sketchbook from "./components/sketchbook.jsx";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-        <Sketchbook color={"red"} bookTitle={"book of gay"}/>
-    </>
-  )
+    return (
+        <>
+            <div className={"bento-grid-container"}>
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+
+                <div className="bento">
+                    <div className={"shelf"}>
+                        <Sketchbook color={"red"} bookTitle={"book 1y"}/>
+                        <Sketchbook color={"green"} bookTitle={"book 2"}/>
+                        <Sketchbook color={"blue"} bookTitle={"book 3"}/>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+    )
 }
 
 export default App
--- popoo/src/components/sketchbook.jsx
+++ popoo/src/components/sketchbook.jsx
@@ -1,11 +1,30 @@
 import "./book.css"
-import PropTypes from "./proptypes"
+import PropTypes from "prop-types";
+import {gsap} from "gsap";
+import {useGSAP} from "@gsap/react";
+import {useRef} from "react";
 
 function Sketchbook({color, bookTitle}) {
+    const container = useRef(null);
+    const tl = useRef(null);
+
+
+
+    useGSAP(()=>{
+        tl.current = gsap.timeline({paused: true})
+            .to(container.current, {scaleX:3})
+    })
+
+    container.current.addEventListener("mouseenter", () =>{
+        tl.current.play()
+    })
+    container.current.addEventListener("mouseout", () =>{
+        tl.current.reverse()
+    })
 
     return (
         <>
-            <div className="book">
+            <div className="book" ref={container}>
                 <div className="book-neck" style={{backgroundColor: color}}>
                     <h1 className="book-title">{bookTitle}</h1>
                 </div>
