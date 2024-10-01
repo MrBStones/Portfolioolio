f17ec31e60aaab214ebd86aca65cf531f2cbe7aa
MrBStones <70972115+MrBStones@users.noreply.github.com>
2024-10-01 17:44:07 +0200

    okay typescript can suck my balls for this

--- appear/src/App.tsx
+++ appear/src/App.tsx
@@ -1,22 +1,24 @@
 import Book from "./components/Book.tsx";
 import './App.css'
 
-
 function App() {
 
     return (
         <>
 
-            <div className="flex-container">
-                <h1>
-                    hello world
-                </h1>
+        <div className="flex-container">
+            <h1>
+                hello world
+            </h1>
+            <div className={"bento"} style={{width: "1000px"}}>
                 <div className="bookshelf-container">
                     <Book bookColor="red" text={"Book1"}/>
                     <Book bookColor="green" text={"Book2"}/>
                     <Book bookColor="blue" text={"Book3"}/>
                 </div>
             </div>
+
+        </div>
         </>
     )
 }
--- appear/src/components/Book.tsx
+++ appear/src/components/Book.tsx
@@ -1,10 +1,47 @@
-import './book.css'
+import gsap from "gsap";
+import {useGSAP} from '@gsap/react';
 
-function Book({bookColor, text} : {bookColor: string, text: string}) {
+import './book.css'
+import {useRef} from "react";
+
+function Book({bookColor, text}: { bookColor: string, text: string }) {
+    const fullBook = useRef<HTMLDivElement>(null);
+    const bookSide = useRef<HTMLDivElement>(null);
+
+    useGSAP((_context, contextSafe ) => {
+
+        const tl = gsap.timeline()
+            .to(bookSide.current, {rotationY: -45})
+            .to(fullBook.current, {scaleX: 1, x: 70}, '<')
+            .reverse()
+
+        const onHover = contextSafe(() => {
+            tl.play().reversed()
+        });
+
+        const unHover = contextSafe(() => {
+            tl.reversed()
+            console.log("unhovered")
+        })
+
+        fullBook.current.addEventListener("mouseenter", onHover);
+        fullBook.current.addEventListener("mouseleave", unHover);
+
+
+
+
+
+
+    }, []);
+
     return (
         <>
-            <div className={"sketchbook"} style={{backgroundColor: bookColor}}>
-                {text}
+            <div className={"book"} ref={fullBook}>
+                <div className={"book-neck"} ref={bookSide} style={{backgroundColor: bookColor}}>
+                    <h1 className={"rotate"}>{text}</h1>
+                </div>
+                <div className={"book-side"} style={{backgroundColor: bookColor}}></div>
+
             </div>
         </>
     )
