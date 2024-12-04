e1fc0b44012ea7ab5e329bd3c209be6e1ce2a644
MrBStones <bjom@itu.dk>
2024-12-04 15:23:18 +0100

    scaffolding of page

---
+++ prort/src/app/_components/bglogo.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import Image from "next/image";
+
+export default function BgLogo() {
+    return (
+
+        <div className={"size-128 opacity-10 "}>
+            <Image
+                src={"/scrunchesvgwhite.svg"}
+                alt={"Scrunches Logo"}
+                height={500}
+                width={500}
+                className={"p-2"}
+            />
+        </div>
+    );
+}
--- prort/src/app/page.tsx
+++ prort/src/app/page.tsx
@@ -5,10 +5,12 @@ import Ratio from "~/app/_components/ratio";
 import Logo from "~/app/_components/logo";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import Image from 'next/image'
+import BgLogo from "~/app/_components/bglogo";
 
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "is here!" });
+  const hello = await api.post.hello({ text: "is here! Scroll ⇓" });
   const session = await auth();
 
   if (session?.user) {
@@ -17,23 +19,59 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#454545] to-[#000000] text-white select-none">
-        <div className="fixed top-9 left-10"><Logo/></div>
-        <div className="fixed top-12 right-10 "><Nav hasSession={session != null}/></div>
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <br/>
-          <h1 className="text-2xl text-white ">
-            Hello (portfolio in progress)
-          </h1>
+      <main
+          className="flex min-h-screen select-none flex-col items-center justify-center bg-gradient-to-b from-[#454545] to-[#000000] text-white -z-20">
+        <div className="fixed left-10 top-9 z-10">
+          <Logo/>
+        </div>
+        <div className="fixed right-10 top-12 z-10">
+          <Nav hasSession={session != null}/>
+        </div>
+        <div className="fixed right-0 bottom-0 z-0">
+          <BgLogo/>
+        </div>
+
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 z-0">
+          <div className={"container flex flex-col h-sc items-center justify-center"} style={{height: "70vh"}}>
+            <h1 className="text-7xl text-white">
+              Hello (portfolio in progress)
+            </h1>
+          </div>
           <p className="text-2xl text-white">
             {hello ? hello.greeting : "Loading tRPC query..."}
           </p>
-          <Ratio leftText={"～(■_■)～♪"} rightText={"(づ ᴗ _ᴗ)づ♡"} odd={false}/>
-          <Ratio leftText={"♪((ヽ(ᐛ)ﾉ))♬"} rightText={"♪(๑ᴖ◡ᴖ๑)♪"} odd={true}/>
-          <Ratio leftText={"( 〃● ₃● ) ~"} rightText={"ヾ(⌐■_■)ノ♪"} odd={false}/>
-          <Ratio leftText={"♪♪♪ ヽ( ᐖゞ)"} rightText={"☆ﾟ°˖* ᕕ( ᐛ )ᕗ"} odd={true}/>
-          <Ratio leftText={"O(∩_∩)O"} rightText={"ヽ( ▀̿ Ĺ̯ ▀̿)ノ♪♬"} odd={false}/>
 
+          <div className="container flex flex-row h-sc h-128">
+            <Image src={"/hisketch.png"} width={694} height={841} alt={"Sketch of scrunches"} objectFit={'contain'}
+                   style={{width: 'auto', height: '100%',}}/>
+
+          </div>
+
+          <div className={"container flex flex-row h-sc h-128 bg-wenge rounded-xl shadow-inner shadow-black"}>
+            <p className={"p-5 text-5xl"}>BOOKSHELF</p>
+          </div>
+
+          <Ratio
+              leftText={"～(■_■)～♪"}
+              rightText={"(づ ᴗ _ᴗ)づ♡"}
+              odd={false}
+          />
+          <Ratio leftText={"♪((ヽ(ᐛ)ﾉ))♬"} rightText={"♪(๑ᴖ◡ᴖ๑)♪"} odd={true}/>
+          <Ratio
+              leftText={"( 〃● ₃● ) ~"}
+              rightText={"ヾ(⌐■_■)ノ♪"}
+              odd={false}
+          />
+          <Ratio
+              leftText={"♪♪♪ ヽ( ᐖゞ)"}
+              rightText={"☆ﾟ°˖* ᕕ( ᐛ )ᕗ"}
+              odd={true}
+          />
+          <Ratio
+              leftText={"O(∩_∩)O"}
+              rightText={"ヽ( ▀̿ Ĺ̯ ▀̿)ノ♪♬"}
+              odd={false}
+          />
 
           {session?.user && <LatestPost/>}
         </div>
