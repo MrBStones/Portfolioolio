89ed383c61bbf2c32bc8150409ba6f438e178959
MrBStones <70972115+MrBStones@users.noreply.github.com>
2025-03-10 17:34:52 +0100

    more anim

--- prort/src/app/_components/nav.tsx
+++ prort/src/app/_components/nav.tsx
@@ -1,38 +1,75 @@
 "use client"
 
 import Link from "next/link";
-import {useState} from "react";
+import {useState, useRef} from "react";
 import Hamburger from "~/app/_components/hamburger";
+import gsap from "gsap";
+import {useGSAP} from "@gsap/react";
+import {TextPlugin} from "gsap/TextPlugin";
+import { text } from "stream/consumers";
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(TextPlugin);
 
 
 export default function Nav() {
+    const container = useRef<HTMLDivElement>(null);
     const [isOpen, setOpen] = useState(false);
+
+    const containerDefault = {width: 65, height: 65};
+
+    useGSAP(() => {
+        const tl = gsap.timeline();
+        const tl2 = gsap.timeline({repeat: -1, repeatDelay: 5, delay: 5});
+        if (isOpen) {
+            tl.from("#nav", {duration: 0.25, width: containerDefault.width, height: containerDefault.height, ease: "power2.in"})
+            .to("#nav", {duration: 0.25, width: 400, height: 200, ease: "power2.out"}, "<")
+            .to("#nav p", {duration: 0.25, opacity: 1, stagger: 0.1, ease: "power2.out"}, "<");
+            
+            tl2.to("#nav p", {duration: 0.6, stagger: 0.6, text: "CLICK ME CLICK ME CLICK ME CLICK ME "})
+            .to("#nav p", {duration: 1, stagger: 0.6, text: "PLACEHOLDER LINK NAME →"},);
+        } else {
+            tl.to("#nav p", {duration: 0.15, opacity: 0, stagger: 0.1, ease: "power2.in"})
+            .to("#nav", {duration: 0.25, width: containerDefault.width , height: containerDefault.height, ease: "power2.inOut"}, "<")
+
+            tl2.clear();
+            
+        }
+    }, [isOpen] );
+
+
 
 
     return (
-        <div
-            className={"container backdrop-filter flex flex-col p-2 rounded-xl backdrop-blur-xl bg-dark/50 gap-2 divide-y divide-hero"}>
+        <div ref={container}>
+            <div id="nav"
+                className={"container backdrop-filter flex flex-col p-2 rounded-xl backdrop-blur-xl bg-dark/50 gap-2 divide-y divide-hero overflow-hidden"}>
 
 
-            <div className="container backdrop-filter flex flex-row items-center justify-end gap-2">
-                <Hamburger toggled={isOpen} toggleAction={setOpen} />
-                {/*<p className="text-center text-2xl text-white">
-                    {session && <span>Logged in as {session.user?.name}</span>}
-                </p>*/}
+                <div className="container backdrop-filter flex flex-row items-center justify-end gap-2">
+                    <Hamburger toggled={isOpen} toggleAction={setOpen} />
+                    {/*<p className="text-center text-2xl text-white">
+                        {session && <span>Logged in as {session.user?.name}</span>}
+                    </p>*/}
+                </div>
+                <p className="text-left text-2xl text-light text-nowrap">
+                    PLACEHOLDER LINK NAME →
+                </p>
+                <p className="text-left text-2xl text-light text-nowrap">
+                    PLACEHOLDER LINK NAME →
+                </p>
+                <p className="text-left text-2xl text-light text-nowrap">
+                    PLACEHOLDER LINK NAME →
+                </p>
             </div>
-            <FakeLink isOpen={isOpen}/>
-            <FakeLink isOpen={isOpen}/>
-            <FakeLink isOpen={isOpen}/>
         </div>
-
-
     )
 }
 
-function FakeLink({isOpen}: {isOpen: boolean }) {
-    if (isOpen){
+function FakeLink({isVisible}: {isVisible: boolean }) {
+    if (isVisible){
         return (
-            <p className="text-center text-2xl text-white">
+            <p className="text-left text-2xl text-light text-nowrap">
                 PLACEHOLDER LINK NAME →
             </p>
         )
--- prort/src/app/_components/speechwimg.tsx
+++ prort/src/app/_components/speechwimg.tsx
@@ -2,16 +2,47 @@
 
 import Image from 'next/image'
 import SpeechBubble from "~/app/_components/speechBubble";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import gsap from "gsap";
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger) 
 
 export default function SpeechWithImg() {
+    const container = useRef<HTMLDivElement>(null);
     const [topIsMouseOver, setTopIsMouseOver] = useState(false);
     const [botIsMouseOver, setBotIsMouseOver] = useState(false);
     const [mouseE, setMouseE] = useState<React.MouseEvent>();
 
+    useGSAP(() => {
+        gsap.from(".container:nth-child(1)", {
+            scrollTrigger: {
+                trigger: ".container:nth-child(1)",
+                start: "top 80%",
+                end: "bottom 20%",
+                toggleActions: "play none none reverse",
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+        })
+        gsap.from(".container:nth-child(2)", {
+            scrollTrigger: {
+                trigger: ".container:nth-child(2)",
+                start: "top 80%",
+                end: "bottom 20%",
+                toggleActions: "play none none reverse",
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+        });
+    }, {scope: container});
+
     return (
-        <div>
-            <div className="container flex flex-row h-sc h-[70vh] place-items-center" onMouseEnter={(e) => { setTopIsMouseOver(true); setMouseE(e); }} onMouseLeave={() => setTopIsMouseOver(false)} onMouseMove={(e) => { setMouseE(e); }} >
+        <div ref={container} className='w-full flex flex-col gap-12'>
+            <div className="container flex flex-row h-sc h-128 place-items-left" onMouseEnter={(e) => { setTopIsMouseOver(true); setMouseE(e); }} onMouseLeave={() => setTopIsMouseOver(false)} onMouseMove={(e) => { setMouseE(e); }} >
                 <Image 
                     src={"/hisketch.png"} 
                     width={694} 
@@ -26,7 +57,7 @@ export default function SpeechWithImg() {
                     left={true}/>
             </div>
 
-            <div className="container flex flex-row h-sc h-[70vh] place-items-center" onMouseEnter={(e) => { setBotIsMouseOver(true); setMouseE(e); }} onMouseLeave={() => setBotIsMouseOver(false)} onMouseMove={(e) => { setMouseE(e); }} >
+            <div className="container flex flex-row h-sc h-128 place-items-end justify-end" onMouseEnter={(e) => { setBotIsMouseOver(true); setMouseE(e); }} onMouseLeave={() => setBotIsMouseOver(false)} onMouseMove={(e) => { setMouseE(e); }} >
                 <SpeechBubble 
                     text1={"This is where"} 
                     text2={"all my stuff is!"} 
