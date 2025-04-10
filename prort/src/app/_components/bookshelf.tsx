b190e7ae8c3e24a32db203d6d94b70b6a8bb35c4
MrBStones <bjom@itu.dk>
2025-04-10 17:34:17 +0200

    added temp anim

--- prort/src/app/_components/bookshelf.tsx
+++ prort/src/app/_components/bookshelf.tsx
@@ -1,8 +1,39 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import BsItem from "./bsitem";
 
 export default function Bookshelf() {
+
+  
+  useGSAP(() =>{
+    let targetsTop = gsap.utils.toArray("#scrollTop");
+    let targetsBot = gsap.utils.toArray("#scrollBot");
+
+    const tl = gsap.timeline({repeat: -1});
+    // carousel animation
+    tl.to(targetsTop, {
+      duration: 3,
+      x: -412,
+      ease: "linear",
+    })
+    .to(targetsBot, {
+      duration: 3,
+      x: 412,
+      ease: "linear",
+    }, "<")
+    .call(() => {
+      
+    }
+    )
+    
+
+
+
+  });
+
+
   return (
     <div
       className={
@@ -13,14 +44,14 @@ export default function Bookshelf() {
         <p className={"p-5 text-5xl"}>BOOKSHELF</p>
       </div>
       <div className="container flex h-full flex-col justify-center gap-3">
-        <div className="container flex flex-row justify-center gap-3 overflow-clip">
+        <div id="scrollTop" className="container flex flex-row justify-center gap-3 overflow-visible">
           <BsItem num="01" title="CHIRP" description="clone of twitter" />
           <BsItem num="03" title="CHIRP" description="clone of twitter" />
           <BsItem num="05" title="CHIRP" description="clone of twitter" />
           <BsItem num="07" title="CHIRP" description="clone of twitter" />
           <BsItem num="09" title="CHIRP" description="clone of twitter" />
         </div>
-        <div className="container flex flex-row justify-center gap-3 overflow-clip">
+        <div id="scrollBot" className="container flex flex-row justify-center gap-3 overflow-visible">
           <BsItem num="02" title="CHIRP" description="clone of twitter" />
           <BsItem num="04" title="CHIRP" description="clone of twitter" />
           <BsItem num="06" title="CHIRP" description="clone of twitter" />
