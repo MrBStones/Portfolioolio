7aafd4c57cd6606b0b93326ca0cb1b0a913e03ae
MrBStones <70972115+MrBStones@users.noreply.github.com>
2025-04-12 23:51:13 +0200

    asd

--- next.config.js
+++ next.config.js
@@ -27,6 +27,15 @@ const config = {  /**
     */
    images: {
      unoptimized: true,
-   },};
+   },
+  
+   env: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
+    AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  };
+
 
 export default config;
