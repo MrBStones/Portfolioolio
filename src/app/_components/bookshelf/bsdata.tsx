d89cc55fd2807751abdc817328d6aaeb28da6c2a
MrBStones <70972115+MrBStones@users.noreply.github.com>
2025-08-16 01:21:37 +0200

    added more projects

--- src/app/_components/bookshelf/bsdata.tsx
+++ src/app/_components/bookshelf/bsdata.tsx
@@ -36,7 +36,7 @@ export const bsData: bsItemProps[] = [
 export const projectsData: bsDetailViewProps[] = [
   {
     description:
-      "This is a project was created during the third semester of university. The site is written in c# using asp.net and is hosted on azure. Bla ",
+      "This project was created during the third semester of university. The site is written in C# using ASP.NET and is hosted on Azure.",
 
     linkOneText: "GitHub repo →",
     linkOne: "https://github.com/ITU-BDSA2024-GROUP9/Chirp",
@@ -53,7 +53,7 @@ export const projectsData: bsDetailViewProps[] = [
   },
   {
     description:
-      "This is a project was created during the second semester of university. The program is created using java and javafx. It uses openstreetmap to display a map of Denmark with markers for different locations, and navigigation.",
+      "This project was created during the second semester of university. The program is created using Java and JavaFX. It uses OpenStreetMap to display a map of Denmark with markers for different locations and navigation.",
     linkOneText: "Project",
     linkTwoText: "Unavailable",
     image: "mapofdk.png",
@@ -67,7 +67,7 @@ export const projectsData: bsDetailViewProps[] = [
   },
   {
     description:
-      "This is a project was created during the fourth semester of university. The app is created using kotlin and android studio. It uses firebase to store and create events, adn native android components to create a slick UI.",
+      "This project was created during the fourth semester of university. The app is created using Kotlin and Android Studio. It uses Firebase to store and create events, and native Android components to create a slick UI.",
     linkOneText: "GitHub Repo →",
     linkOne: "https://github.com/MrBStones/CopenhagenBuzz",
     linkTwoText: "",
@@ -77,7 +77,62 @@ export const projectsData: bsDetailViewProps[] = [
       title: "COPENHAGEN BUZZ",
       description: "event planner app",
     },
-    technologies: ["Android", "Kotlin", "Firebase", "Google Maps"],
+    technologies: ["android", "kotlin", "firebase", "google maps"],
     icon: "smartphone_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg",
+  },
+  {
+    description:
+      "This project was created during the fourth semester of university. Our group was tasked with creating a system that automatically generates market report emails and sends them to users. The project was created in collaboration with CM-Navigator. Project organization was handled using the Scrum framework.",
+    linkOneText: "Project",
+    linkTwoText: "Unavailable",
+    image: "cmnav.png",
+    bsItem: {
+      num: "04",
+      title: "MARKET REPORTS",
+      description: "email generation system",
+    },
+    technologies: ["typescript", "react", "neondb", "prisma"],
+    icon: "language_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg",
+  },
+  {
+    description:
+      "I like messing about with this website from time to time. It has been a fun side project to work on when I have had some downtime. In the process, it taught me a lot of TypeScript and React. It feels a bit backwards to create the portfolio before I have anything to show, but I guess that's just how it is. :P",
+    linkOneText: "GitHub repo →",
+    linkOne: "https://github.com/MrBStones/Portfolioolio",
+    linkTwoText: "Go to site →",
+    linkTwo: "https://portfolioolio.vercel.app/",
+    image: "scrunchesvgwithbg.svg",
+    bsItem: {
+      num: "05",
+      title: "PORTFOLIO SITE",
+      description: "this site ur on rn",
+    },
+    technologies: ["typescript", "react", "tailwind", "gsap"],
+    icon: "language_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg",
+  },
+  {
+    description:
+      "I don't have this set up yet, but I draw a little, and this is where they are going to go. (The little guy in the image is Scrunches. Look at him go!)",
+    linkOneText: "Sketchbooks →",
+    linkOne: "https://github.com/MrBStones/Portfolioolio",
+    linkTwoText: "",
+    image: "drawing.png",
+    bsItem: { num: "06", title: "SKETCHES", description: "art i made" },
+    technologies: ["pen", "paper", "samsung galaxy tab s8"],
+    icon: "brush_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg",
+  },
+  {
+    description: "This game sucks. I made it in two days.",
+    linkOneText: "Go to itch →",
+    linkOne: "https://mrstones.itch.io/regurgitate",
+    linkTwoText: "",
+    image: "https://img.itch.zone/aW1nLzIyNTAyOTg4LnBuZw==/original/dTUi69.png",
+    bsItem: {
+      num: "07",
+      title: "REGURGITATE",
+      description: "gmtk game jam 2025",
+    },
+    technologies: ["c#", "godot"],
+    icon: "desktop_windows_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg",
   },
 ];
